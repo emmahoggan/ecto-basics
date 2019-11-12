d4b6bba34f3c51345fed749e21a3cb61f0995a8e
Emma Hoggan <emma.hoggan@podium.com>
2019-11-12 09:35:34 -0700

    Added method to book service with book availability query.

--- library_catalog/lib/library_catalog/book_service.ex
+++ library_catalog/lib/library_catalog/book_service.ex
@@ -1,4 +1,12 @@
 defmodule LibraryCatalog.BookService do
+
+  import Ecto.Query, only: [from: 2]
+
+  def get_book_availability(title) do
+    query = from b in "books", where: b.title == ^title, select: b.available
+    LibraryCatalog.Repo.all(query)
+  end
+
   def add_book(params) do
     %LibraryCatalog.Book{}
     |> LibraryCatalog.Book.changeset(params)
